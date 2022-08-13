9c44cd61fd1126a0e98be71140144a3370967cc3
Debora Silva <deborafllyon@gmail.com>
2022-08-13 17:12:09 -0300

    CRUD criado no prpjeto app-employees

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyARqJ4CIdWVpf90SJshbpWo3Gek_583-gU",
-    authDomain: "app-employees-1b68b.firebaseapp.com",
-    projectId: "app-employees-1b68b",
-    storageBucket: "app-employees-1b68b.appspot.com",
-    messagingSenderId: "380832212638",
-    appId: "1:380832212638:web:ea62d437b2fb4367513d8f"
+    apiKey: "AIzaSyCDQ_FFw0otdZF-recUIimp5hB2UznjnA0",
+    authDomain: "app-employees-8fcbc.firebaseapp.com",
+    projectId: "app-employees-8fcbc",
+    storageBucket: "app-employees-8fcbc.appspot.com",
+    messagingSenderId: "773710938404",
+    appId: "1:773710938404:web:796ab3619fbcb13d7df3b1"
   }
 };
