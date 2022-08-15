72d01966914364c4047b55be003959bc58dae259
Debora Silva <deborafllyon@gmail.com>
2022-08-15 18:20:12 -0300

    Atualização environment.ts & prod ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDQ_FFw0otdZF-recUIimp5hB2UznjnA0",
+    authDomain: "app-employees-8fcbc.firebaseapp.com",
+    projectId: "app-employees-8fcbc",
+    storageBucket: "app-employees-8fcbc.appspot.com",
+    messagingSenderId: "773710938404",
+    appId: "1:773710938404:web:796ab3619fbcb13d7df3b1"
+  }
 };
